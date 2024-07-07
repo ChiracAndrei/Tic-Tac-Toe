f32a64c0eee80586d66320ed065847bfddee835a
Chirac Andrei <chirac_andrei@yahoo.com>
2024-07-07 13:23:01 +0300

    save game on refresh

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
 
   return (
     <>
-
       <Names></Names>
-
       <Board></Board>
     </>
   );
--- src/Board.js
+++ src/Board.js
@@ -4,18 +4,42 @@ import './App.css';
 
 function Board() {
 
-    const [box, setBox] = useState(Array(9).fill(null));
-    let [turn, setTurn] = useState(true);
+    const [box, setBox] = useState(() => {
+        return (JSON.parse(localStorage.getItem('box')))
+    });
+
+    let [turn, setTurn] = useState(() => {
+        return (JSON.parse(localStorage.getItem("turn")))
+    });
+
+    const [winner, setWinner] = useState(() => {
+        return (JSON.parse(localStorage.getItem("winner")))
+    })
+
+
+    useEffect(() => {
+        localStorage.setItem("box", JSON.stringify(box))
+        localStorage.setItem("turn", JSON.stringify(turn))
+        localStorage.setItem("winner", JSON.stringify(winner))
+
+    }, [box, turn, winner]);
+
+
+
     function Box({ value, onBoxClick }) {
 
         return <button className="box" onClick={onBoxClick}>{value}</button>;
     }
-    function Winner(box) {
+
+
+
+    function calculateWinner(box) {
         const options = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [2, 4, 6]
         ];
+
         for (let i = 0; i < options.length; i++) {
             const [a, b, c] = options[i];
             if (box[a] && box[a] === box[b] && box[a] === box[c]) {
@@ -25,19 +49,25 @@ function Board() {
         return null;
     }
 
-    let status;
-    if (Winner(box)) {
-        status = "Castigatorul este " + Winner(box);
 
-        // <div id="winer_baner">Castigatorul rundei este {(turn ? "X" : "O")}</div>
+
+
+    let status;
+    if (calculateWinner(box)) {
+        status = "Castigatorul este " + calculateWinner(box);
+
     } else {
         status = "Este randul lui: " + (turn ? "X" : "O");
     }
 
+
+
+
+
+
     function Click(i) {
-        if (box[i] || Winner(box)) {
-            return;
-        }
+
+        if (box[i] || winner) return;
         const nextMove = box.slice();
         if (turn) {
             nextMove[i] = "X"
@@ -47,13 +77,17 @@ function Board() {
 
         setBox(nextMove);
         setTurn(!turn)
+        setWinner(calculateWinner(nextMove))
     }
+
+
+
+
     function Reset() {
-
-
         setBox(Array(9).fill(null));
         turn = true
         setTurn(turn)
+        setWinner(null)
 
     }
 
